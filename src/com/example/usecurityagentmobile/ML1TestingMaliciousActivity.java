9fde28b7c8e665c012f7018ddaf72558a9ad6455
kylee <yklee06@gmail.com>
2013-10-23 12:56:38 +0900

    액티비티 추가

--- src/com/example/usecurityagentmobile/ML1TestingMaliciousActivity.java
+++ src/com/example/usecurityagentmobile/ML1TestingMaliciousActivity.java
@@ -2,7 +2,10 @@ package com.example.usecurityagentmobile;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class ML1TestingMaliciousActivity extends Activity {
 
@@ -10,6 +13,17 @@ public class ML1TestingMaliciousActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ml1_testing_malicious);
+		
+		//검사하기
+		Button bViewResultDetail = (Button)findViewById(R.id.button_view_result_detail);
+		bViewResultDetail.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(),ResultMaliciousTestingActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 	@Override
